e4a33a3bd622a5388c1cd05659d5a77b6979e9a3
nurul hassan <nurulhassan2000@gmail.com>
2019-02-27 16:15:16 +0530

    some work

--- js/jharoka.js
+++ js/jharoka.js
@@ -9,9 +9,9 @@ $(document).ready(() => {
 					
 					console.log(DocWidth)
 					if(DocWidth>=800){
-					$('.jharokha_img').attr("src","elmt/jharokha_gate.png")
-					$('.jharokha_img').addClass("jharokha1")
-					$('.jharokha_img').removeClass("jharokha2")
+					$('.jharokha_img').attr("src","elmt/jharokha1old.png")
+					// $('.jharokha_img').addClass("jharokha1")
+					// $('.jharokha_img').removeClass("jharokha2")
 					$('.jharokha').addClass("jharoka-back")
 					$('.pillar').addClass('inactive')
 					// $('.gate-contain').remove()
@@ -48,16 +48,16 @@ $(document).ready(() => {
 					console.log(DocWidth)
 					console.log(DocHeight)
 					if(DocWidth>=800){
-					$('.jharokha_img').attr("src","elmt/jharokha_gate.png")
-					$('.jharokha_img').addClass("jharokha1")
-					$('.jharokha_img').removeClass("jharokha2")
+					$('.jharokha_img').attr("src","elmt/jharokha1old.png")
+					// $('.jharokha_img').addClass("jharokha1")
+					// $('.jharokha_img').removeClass("jharokha2")
 					$('.pillar').addClass('inactive')
 					
 				}
 				else{
 					$('.jharokha_img').attr("src","elmt/jarokha_gate_final.png")
-					$('.jharokha_img').removeClass("jharokha1")
-					$('.jharokha_img').addClass("jharokha2")
+					// $('.jharokha_img').removeClass("jharokha1")
+					// $('.jharokha_img').addClass("jharokha2")
 					$('.pillar').attr('height',DocHeight)
 					$('.pillar').removeClass('inactive')
 					// $('.gate-contain').remove()
